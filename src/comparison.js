3f5a7046aae7a8fc475fcdf27c4976d740c99a03
Maria Oleynikova <oleyn11@mail.ru>
2023-02-11 16:33:35 +0700

    delete space

--- src/comparison.js
+++ src/comparison.js
@@ -1,26 +1,26 @@
 import _ from 'lodash';
 
 const getComparison = (file1, file2) => {
-    const keys1 = Object.keys(file1);
-    const keys2 = Object.keys(file2);
-    const keys = _.union(keys1, keys2);
-    const sortedKeys = _.sortBy(keys);
+  const keys1 = Object.keys(file1);
+  const keys2 = Object.keys(file2);
+  const keys = _.union(keys1, keys2);
+  const sortedKeys = _.sortBy(keys);
 
-    const result = sortedKeys.reduce((acc, key) => {
-        if (!Object.hasOwn(file1, key)) {
-            acc.push(` + ${key}: ${file2[key]} \n`);
-        } else if(!Object.hasOwn(file2, key)) {
-            acc.push(` - ${key}: ${file1[key]} \n`);
-        } else if(file1[key] !== file2[key]) {
-            acc.push(` - ${key}: ${file1[key]} \n + ${key}: ${file2[key]} \n`);
-        } else {
-            acc.push(`   ${key}: ${file1[key]} \n`);
-        }
-        return acc;
-    }, []);
+  const result = sortedKeys.reduce((acc, key) => {
+    if (!Object.hasOwn(file1, key)) {
+      acc.push(` + ${key}: ${file2[key]}\n`);
+    } else if (!Object.hasOwn(file2, key)) {
+      acc.push(` - ${key}: ${file1[key]}\n`);
+    } else if (file1[key] !== file2[key]) {
+      acc.push(` - ${key}: ${file1[key]}\n + ${key}: ${file2[key]}\n`);
+    } else {
+      acc.push(`   ${key}: ${file1[key]}\n`);
+    }
+    return acc;
+  }, []);
 
-    const result1 = `{\n${result.join('')}\n}`;
-    return result1;
+  const result1 = `{\n${result.join('')}}`;
+  return result1;
 };
 
 export default getComparison;
