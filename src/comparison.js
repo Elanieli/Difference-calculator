a3c22ac1fcfb99e023c9e93aeb3889518030a776
Maria Oleynikova <oleyn11@mail.ru>
2023-02-24 13:29:24 +0700

    change src/comparison.js

--- src/comparison.js
+++ src/comparison.js
@@ -6,21 +6,46 @@ const getComparison = (file1, file2) => {
   const keys = _.union(keys1, keys2);
   const sortedKeys = _.sortBy(keys);
 
-  const result = sortedKeys.reduce((acc, key) => {
-    if (!Object.hasOwn(file1, key)) {
-      acc.push(` + ${key}: ${file2[key]}\n`);
-    } else if (!Object.hasOwn(file2, key)) {
-      acc.push(` - ${key}: ${file1[key]}\n`);
-    } else if (file1[key] !== file2[key]) {
-      acc.push(` - ${key}: ${file1[key]}\n + ${key}: ${file2[key]}\n`);
-    } else {
-      acc.push(`   ${key}: ${file1[key]}\n`);
-    }
-    return acc;
-  }, []);
+  const result = sortedKeys.map((key) => {
+    const value1 = file1[key];
+    const value2 = file2[key];
 
-  const result1 = `{\n${result.join('')}}`;
-  return result1;
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return {
+        status: 'nested',
+        key,
+        children: getComparison(value1, value2),
+      };
+    }
+    if (!_.has(file2, key)) {
+      return {
+        status: 'deleted',
+        key,
+        value: value1,
+      };
+    }
+    if (!_.has(file1, key)) {
+      return {
+        status: 'added',
+        key,
+        value: value2,
+      };
+    }
+    if (value1 !== value2) {
+      return {
+        status: 'changed',
+        key,
+        oldValue: value1,
+        newValue: value2,
+      };
+    }
+    return {
+      status: 'unchanged',
+      key,
+      value: value1,
+    };
+  });
+  return result;
 };
 
 export default getComparison;
