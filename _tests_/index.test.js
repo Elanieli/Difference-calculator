cc6ef75c2e2b40c1d3935e801f66063e7f800500
Maria Oleynikova <oleyn11@mail.ru>
2023-02-24 13:27:40 +0700

    change tests

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -10,13 +10,14 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '_fixtures_', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
-const resultJson = readFile('result/result_json.txt');
 const resultStylish = readFile('result/result_stylish.txt');
+const resultJson = readFile('result/result_json.txt');
 
 test.each(['json', 'yml'])('genDiff(%#)', (format) => {
   const file1 = getFixturePath(`file1.${format}`);
   const file2 = getFixturePath(`file2.${format}`);
 
-  expect(genDiff(file1, file2, 'json')).toEqual(resultJson);
   expect(genDiff(file1, file2)).toEqual(resultStylish);
+  expect(genDiff(file1, file2, 'stylish')).toEqual(resultStylish);
+  expect(genDiff(file1, file2, 'json')).toEqual(resultJson);
 });
