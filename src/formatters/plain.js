bdf52dfcc860a762edb77167db5c1a0c842ca590
Maria Oleynikova <oleyn11@mail.ru>
2023-03-01 12:26:57 +0700

    add plain format

---
+++ src/formatters/plain.js
@@ -0,0 +1,38 @@
+import _ from 'lodash';
+
+const formatter = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  if (_.isString(value)) {
+    return `'${value}'`;
+  }
+  return value;
+};
+
+const plain = (resultOfComparison) => {
+  const iter = (currentValue, path) => {
+    const data = currentValue.map((obj) => {
+      const keyPath = (path === '' ? `${obj.key}` : `${path}.${obj.key}`);
+
+      switch (obj.status) {
+        case 'changed':
+          return `Property '${keyPath}' was updated. From ${formatter(obj.oldValue)} to ${formatter(obj.newValue)}`;
+        case 'nested':
+          return iter(obj.children, keyPath);
+        case 'added':
+          return `Property '${keyPath}' was added with value: ${formatter(obj.value)}`;
+        case 'deleted':
+          return `Property '${keyPath}' was removed`;
+        case 'unchanged':
+          return null;
+        default:
+          throw new Error(`${obj.status} is wrong!`);
+      }
+    });
+    return data.filter(Boolean).join('\n');
+  };
+  return iter(resultOfComparison, '');
+};
+
+export default plain;
