15df01d084e4fe474fc28b634711e8f06aafb89d
Maria Oleynikova <oleyn11@mail.ru>
2023-03-01 19:07:34 +0700

    change src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -3,12 +3,12 @@ import plain from './plain.js';
 
 const format = (data, formatName) => {
   switch (formatName) {
-    case 'json':
-      return JSON.stringify(data);
     case 'stylish':
       return stylish(data);
     case 'plain':
       return plain(data);
+    case 'json':
+      return JSON.stringify(data);
     default:
       throw new Error(`${formatName} is wrong format!`);
   }
